19e141bdcc6f54ed5075b955c674f9a6e18fdc47
Sachin Maurya <kumarsachin67671@gmail.com>
2024-12-02 18:21:45 +0530

    constant

---
+++ day2/constant.dart
@@ -0,0 +1,6 @@
+void main() {
+ const int num1 = 30;
+ // we can not assign again also we are use same line we can't change variable
+  //num1 = 40;                                                
+  print(num1);
+}
