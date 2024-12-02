9776c0a93127c35b714d02e20de9951e77b6b1af
Sachin Maurya <kumarsachin67671@gmail.com>
2024-12-02 11:29:41 +0530

    Logical OR oprator

---
+++ day2/logicalAndOp.dart
@@ -0,0 +1,15 @@
+import 'dart:io';
+
+void main() {
+  stdout.write("1.Enter your number:");
+  int num1 = int.parse(stdin.readLineSync()!);
+  stdout.write("2.Enter your second number:");
+  int num2 = int.parse(stdin.readLineSync()!);
+  stdout.write("3.Entere your third number:");
+  int num3 = int.parse(stdin.readLineSync()!);
+  if (num1 > num2 && num1 > num3) {
+    print("num1 is grater:");
+  } else {
+    print("Enter number");
+  }
+}
---
+++ day2/logicalOr.dart
@@ -0,0 +1,16 @@
+import 'dart:io';
+
+void main(){
+   stdout.write("1.Enter your number:");
+  int num1 = int.parse(stdin.readLineSync()!);
+  stdout.write("2.Enter your second number:");
+  int num2 = int.parse(stdin.readLineSync()!);
+  stdout.write("3.Entere your third number:");
+  int num3 = int.parse(stdin.readLineSync()!);
+  // it is check only one condition which is along with this
+  if (num1 > num2 || num1 > num3) {
+    print("num1 is grater:");
+  } else {
+    print("Enter number");
+  }
+}
