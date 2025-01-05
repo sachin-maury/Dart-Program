8560d6ccd1865d296549cdbc312ccc51916e9353
Sachin Maurya <kumarsachin67671@gmail.com>
2025-01-05 09:59:05 +0530

    Map

---
+++ day5/map1.dart
@@ -0,0 +1,11 @@
+class Practice {
+  add() {
+    Map<String, String> mydata = {"Name": "Sachin", "vill": "Bihar"};
+    print(mydata);
+  }
+}
+
+void main() {
+  Practice obj = Practice();
+  obj.add();
+}
