be94dc93e50e2fc971512dbc700f3efcffb2720e
Sachin Maurya <kumarsachin67671@gmail.com>
2025-01-05 10:13:41 +0530

    Int with Str with double

---
+++ day5/mapSI.dart
@@ -0,0 +1,11 @@
+class Test {
+  add() {
+    Map<String, dynamic> mydata = {"Id": 2, "SubId": 4,"Name":"Sachin","Percentage":4.0};
+    print(mydata);
+  }
+}
+
+void main() {
+  Test obj = Test();
+  obj.add();
+}
