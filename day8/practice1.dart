01f26c9ff848bb9db65c50cecce9094041baf382
Sachin Maurya <kumarsachin67671@gmail.com>
2025-01-09 07:42:10 +0530

    leap year program

--- day8/practice1.dart
+++ day8/practice1.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 void main() {
   stdout.write("Enter your year = ");
   int year = int.parse(stdin.readLineSync()!);
+  // here is our condition
   if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
     print("$year is a leap year!");
   } else {
