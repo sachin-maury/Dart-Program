a9ac19a7a6b9e034226f9d6577fa2c4a9f5e00f1
Sachin Maurya <kumarsachin67671@gmail.com>
2025-04-05 11:20:16 +0530

    Function with parameter

---
+++ Function Dart/myClass1.dart
@@ -0,0 +1,12 @@
+void main() {
+  var MyC = Myclass1();
+  MyC.MyHome('Shakti sing');
+  MyC.MyHome('Sadanand Sing');
+  MyC.MyHome('Sachin Singh');
+}
+
+class Myclass1 {
+  void MyHome(String name) {
+    print(name);
+  }
+}
